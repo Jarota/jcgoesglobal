44e2c727654912c3d5b6303071321372a17a90cb
jimt <jamesrobtait@gmail.com>
2025-09-07 18:02:14 +0800

    tweak animations

--- static/index.js
+++ static/index.js
@@ -42,13 +42,15 @@ function renderCarousel(id) {
   carousel.classList.add('shown')
 
   // Also prevent scrolling in the background
-  document.body.style.overflowY = 'hidden'
+  document.body.style.overflow = 'hidden'
+  document.documentElement.style.overflow = 'hidden'
 }
 
 function hideCarousel() {
-    carousel.classList.remove('shown')
-    carousel.classList.add('hidden')
-    document.body.style.overflowY = '' // Re-enable scrolling too
+  carousel.classList.remove('shown')
+  carousel.classList.add('hidden')
+  document.body.style.overflow = '' // Re-enable scrolling too
+  document.documentElement.style.overflow = ''
 }
 
 function renderTimeline() {
@@ -81,27 +83,28 @@ function renderTimeline() {
 }
 
 function renderPics(id, pics) {
-    if (!pics || pics.length === 0) {
-      return ''
-    }
+  if (!pics || pics.length === 0) {
+    return ''
+  }
 
-    let picsHtml = ''
-    let degrees = -10
-    let z = -1
-    pics.forEach((pic) => {
-      picsHtml += `<img class="pic"
-        src="assets/pics/${pic}" data-zoom="${id}"
-        style="transform: rotate(${degrees}deg); z-index: ${z};"
-      >`
-      degrees = (degrees > 0 ? degrees + 10 : degrees - 10) * -1
-      z -= 1
-    })
+  let picsHtml = ''
+  let degrees = -10
+  let z = -1
+  pics.forEach((pic) => {
+    picsHtml += `<img class="pic"
+      src="assets/pics/${pic}" data-zoom="${id}"
+      style="transform: rotate(${degrees}deg); z-index: ${z};"
+    >`
 
-    return `
-      <div class="pics" data-zoom="${id}">
-        ${picsHtml}
-      </div>
-    `
+    degrees = (degrees > 0 ? degrees + 10 : degrees - 10) * -1
+    z -= 1
+  })
+
+  return `
+    <div class="pics" data-zoom="${id}">
+      ${picsHtml}
+    </div>
+  `
 }
 
 /* SECRET NAV HANDLERS */
